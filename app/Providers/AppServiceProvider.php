a43668bd8af6e7f4c832023861279bb8dea27532
EC2 Default User <ec2-user@ip-172-31-6-83.us-east-2.compute.internal>
2019-11-19 09:18:37 +0000

    herokuにアップしてみる

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,21 +8,18 @@ class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
     public function register()
     {
-        //
     }
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
     public function boot()
     {
-        //
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
